c49f3c0618bfb5b56cabdb16ab5e0bdd09429171
SamiaSabaMim <samiatusesabamim@gmail.com>
2026-07-05 21:27:14 +0600

    Sheet 02. B. Even Numbers

---
+++ Competetive Programming/ASSUIT ICPC Prep/Mim/Sheet  #02 (Loops)/B_Even_Numbers.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int N;
+    cin >> N;
+
+    for(int i=1; i<=N; i++ )
+    {
+        if(i%2==0)
+        {
+            cout<< i <<'\n';
+        }
+        
+    }
+    if(N<2)
+    {
+        cout<< -1 <<endl;
+    }
+}
