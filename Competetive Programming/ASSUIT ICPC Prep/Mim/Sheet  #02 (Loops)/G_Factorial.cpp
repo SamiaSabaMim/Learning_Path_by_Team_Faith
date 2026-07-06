618193f932d8bd0f16d630504be6947a55ae5b5b
SamiaSabaMim <samiatusesabamim@gmail.com>
2026-07-06 14:17:20 +0600

    Sheet 02. G. Factorial

---
+++ Competetive Programming/ASSUIT ICPC Prep/Mim/Sheet  #02 (Loops)/G_Factorial.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int T;
+    cin >> T;
+
+    for(int i = 1; i <= T; i++)
+    {
+        int N;
+        cin >> N;
+
+        long long fact = 1;
+
+        for(int j = 1; j <= N; j++)
+        {
+            fact = fact * j;
+        }
+
+        cout << fact << endl;
+    }
+
+    return 0;
+}
