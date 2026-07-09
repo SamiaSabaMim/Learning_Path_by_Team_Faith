dfc62859d18d760a8df99aeefe8bc447108f9d1a
SamiaSabaMim <samiatusesabamim@gmail.com>
2026-07-09 20:45:34 +0600

    Sheet 02. N. Numbers Histogram

---
+++ Competetive Programming/ASSUIT ICPC Prep/Mim/Sheet  #02 (Loops)/N_Numbers_Histogram.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    char S;
+    cin >> S;
+    int N;
+    cin >>N;
+
+    for(int i =0;i<N;i++)
+    {
+        int X;
+        cin>>X;
+
+        for(int j=0;j<X;j++)
+    {
+        cout<<S;
+    }
+    cout<<'\n';
+    }
+}
