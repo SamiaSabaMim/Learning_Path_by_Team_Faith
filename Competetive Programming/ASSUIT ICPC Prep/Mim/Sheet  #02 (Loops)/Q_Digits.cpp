0091fcf20b7cbbd1cb83fedebfb430affbcc483d
SamiaSabaMim <samiatusesabamim@gmail.com>
2026-07-10 16:57:56 +0600

    Sheet 02. Q. Digits

---
+++ Competetive Programming/ASSUIT ICPC Prep/Mim/Sheet  #02 (Loops)/Q_Digits.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+int main ()
+{
+    int T;
+    cin >> T;
+    while(T--)
+    {
+        int N;
+        cin>> N;
+
+        if(N==0)
+        {
+            cout<< 0 <<'\n';
+            continue;
+        }
+        while(N>0)
+        {
+            cout<<N%10<<" ";
+            N = N/10;
+        }
+        cout<<'\n';
+    }
+
+}
