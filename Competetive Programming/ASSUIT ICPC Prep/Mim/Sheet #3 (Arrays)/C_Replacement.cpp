7e37591eac3faaf1031bad543b4abe2de208c9a6
SamiaSabaMim <samiatusesabamim@gmail.com>
2026-07-14 15:37:37 +0600

    Sheet 03. C. Replacement

---
+++ Competetive Programming/ASSUIT ICPC Prep/Mim/Sheet #3 (Arrays)/C_Replacement.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int N;
+    cin >> N;
+
+    int A[N];
+
+    for(int i=0; i<N; i++)
+    {
+        cin >> A[i];
+
+        if(A[i] >0)
+        {
+            cout<< 1 << " ";
+        }
+        else if(A[i]<0)
+        {
+            cout << 2 << " ";
+        }
+        else{
+            cout << 0 << " " ;
+        }
+    }
+}
