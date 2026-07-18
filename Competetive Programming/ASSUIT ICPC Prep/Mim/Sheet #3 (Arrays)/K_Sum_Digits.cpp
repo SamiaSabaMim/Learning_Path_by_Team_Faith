e749566393c48c2b3031d47c1b8b7bf9c819203f
SamiaSabaMim <samiatusesabamim@gmail.com>
2026-07-18 19:19:49 +0600

    Sheet 03. K. Sum Digits

---
+++ Competetive Programming/ASSUIT ICPC Prep/Mim/Sheet #3 (Arrays)/K_Sum_Digits.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int N;
+    cin >> N;
+
+    string digits;
+    
+    cin >> digits;
+
+    int sum = 0;
+    for (int i = 0; i < N; i++) {
+        sum += digits[i] - '0'; 
+    }
+   cout << sum << endl;
+    return 0;
+}
